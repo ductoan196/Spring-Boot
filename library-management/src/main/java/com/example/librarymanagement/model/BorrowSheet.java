b90acd6c5d48eb51744282fd80ecf2f2c533400a
ductoan196 <ductoannd96@gmail.com>
2023-05-16 06:55:36 +0700

    update

--- library-management/src/main/java/com/example/librarymanagement/model/BorrowSheet.java
+++ library-management/src/main/java/com/example/librarymanagement/model/BorrowSheet.java
@@ -1,4 +1,17 @@
 package com.example.librarymanagement.model;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class BorrowSheet {
+    int id;
+    Reader reader;
+    
 }
